380ae286e306fd4ee24ea9b043c093876cc6e07a
LuPeBreak <lfbmrj15@gmail.com>
2020-02-20 10:34:53 -0300

    Rota Post ( projects ) Concluida - Criar e armazenar novos projetos - Modificado em relaçao ao desafio: id gerado automaticamente

--- index.js
+++ index.js
@@ -2,17 +2,40 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 const projects=[
   {
-    id: "1",
+    id: 0,
     title: "Novo projeto",
     tasks: ["Nova tarefa"]
   }
 ];
 
+//CRUD
+//Create
+//Cria e armazena novo usuario - ( A funçao foi modificada para gerar o id automaticamente )
+server.post('/projects',(req,res)=>{
+  const project = {
+    id:projects.length,
+    ...req.body,
+  }
+  projects.push(project);
+  return res.json({
+    "message":"projeto criado com sucesso",
+    "projects":projects,
+  })
+});
+
+//Read
+//Retorna todos os usuarios
 server.get('/projects',(req,res)=>{
   return res.json(projects);
 });
+
+//Update
+
+//Delete
 
 
 server.listen(3000);
